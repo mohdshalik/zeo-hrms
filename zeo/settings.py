554f9c6e4870556871509ded7a2e50f1bc74e58b
subina <subina@gmail.com>
2024-12-17 17:03:44 +0530

    add

--- zeo/settings.py
+++ zeo/settings.py
@@ -166,7 +166,7 @@ WSGI_APPLICATION = 'zeo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django_tenants.postgresql_backend',
-        'NAME': 'zeo', 
+        'NAME': 'zeo1', 
         'USER': 'postgres', 
         'PASSWORD': '1234',
         'HOST': '127.0.0.1', 
