69807c6e17c274afd6538f395da6c05beaaafeed
subina <subina@gmail.com>
2025-04-12 13:26:41 +0530

    adds

--- PayrollManagement/apps.py
+++ PayrollManagement/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PayrollmanagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'PayrollManagement'
+    
+    def ready(self):
+        import calendars.signals
